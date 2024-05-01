bfb4d12caa7508a10978c77d1de9a1bfa4045359
Hubert S <hubert.schmidt@microsoft.wsei.edu.pl>
2024-05-01 13:06:30 +0200

    Dodanie kalendarza, cwiczen, trenerow. Wstępna edycja kalendarza

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 const kalendarz = document.getElementById("kalendarz");
+const cwiczenia = document.getElementById("cwiczenia");
+const trenerzy = document.getElementById("trenerzy")
 
 kalendarz.addEventListener("click", function() {
     window.open("./kalendarz.html", "_self");
+});
+
+cwiczenia.addEventListener("click", function() {
+    window.open("./cwiczenia.html", "_self");
+});
+
+trenerzy.addEventListener("click", function() {
+    window.open("./trenerzy.html", "_self");
 });
